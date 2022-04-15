4efaebae5143e055d53420406c9e5b3bd01a807c
tcyuen <98924935+tcyuen@users.noreply.github.com>
2022-04-15 15:24:45 -0700

    Update lin_regress_depress.R

--- lin_regress_depress.R
+++ lin_regress_depress.R
@@ -68,6 +68,16 @@ lin_mod <- lm(betas ~ delta_codon_freqs)
 
 ggplotRegression(lin_mod)
 
+#pval <0.01
+betas <- combined_depress_data[combined_depress_data$pval < 0.01, ]$beta
+
+delta_codon_freqs <- combined_depress_data[combined_depress_data$pval < 0.01, ]$delta
+
+summary(lm(betas ~ delta_codon_freqs))
+lin_mod <- lm(betas ~ delta_codon_freqs)
+
+ggplotRegression(lin_mod)
+
 #pval <0.001
 betas <- combined_depress_data[combined_depress_data$pval < 0.001, ]$beta
 
